cf2953c4fb428e1f963f5ffe409dd6cab9975161
Anshul <anshulaj999@gmail.com>
2025-11-23 04:50:18 +0530

    HAOS 1

---
+++ ha_addon/run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/with-contenv bashio
+
+echo "Starting SyncLyrics..."
+
+# Read config from HA options
+export SPOTIFY_CLIENT_ID=$(bashio::config 'spotify_client_id')
+export SPOTIFY_CLIENT_SECRET=$(bashio::config 'spotify_client_secret')
+export SPOTIFY_REDIRECT_URI=$(bashio::config 'spotify_redirect_uri')
+export SERVER_PORT=$(bashio::config 'server_port')
+
+# Set other defaults for Linux/Docker
+export DESKTOP="Linux"
+export PYTHONUNBUFFERED=1
+
+# Run the app
+python3 sync_lyrics.py
