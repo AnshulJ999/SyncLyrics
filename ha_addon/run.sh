01ede7318598f5ee62c41645994ee8d50db0b00a
Anshul <anshulaj999@gmail.com>
2025-11-23 04:53:54 +0530

    HAOS 2

--- ha_addon/run.sh
+++ ha_addon/run.sh
@@ -8,6 +8,9 @@ export SPOTIFY_CLIENT_SECRET=$(bashio::config 'spotify_client_secret')
 export SPOTIFY_REDIRECT_URI=$(bashio::config 'spotify_redirect_uri')
 export SERVER_PORT=$(bashio::config 'server_port')
 
+# Generate a random secret key for the session
+export QUART_SECRET_KEY="ha-secret-$(date +%s)"
+
 # Set other defaults for Linux/Docker
 export DESKTOP="Linux"
 export PYTHONUNBUFFERED=1
